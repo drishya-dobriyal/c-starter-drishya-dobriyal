a95ca2b8bc5b00276ef9798579f02be78f537f2a
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-03-28 19:11:15 +0530

    Added print fibonacci function

--- loop.c
+++ loop.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int fact(int);
+int fact(int);
 
 int fact(int num ) {
   if(num <= 1) {
@@ -9,9 +10,27 @@ int fact(int num ) {
   return num * fact( num - 1);
 }
 
+int print_fibonacci_series(num) {
+  int prevTerm = -1;
+  int currterm = 1;
+  int numOfTerm = 0;
+  while(numOfTerm != num) {
+    int temp = prevTerm;
+    prevTerm = currterm;
+    currterm = temp + prevTerm;
+    printf(" %d \n", currterm);
+    numOfTerm +=1;
+  }
+  return 0;
+}
+
 int main(void) {
   int num;
   printf("Enter Num to find its factorial : \n");
   scanf("%d",&num);
   printf("The factorial of %d is %d\n", num, fact(num));
+
+  printf("Enter num of terms you want to print fibonacci \n");
+  scanf("%d",&num);
+  print_fibonacci_series(num);
 }
