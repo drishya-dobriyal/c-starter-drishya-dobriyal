ef77c9d4a981efa857f9a235aeb832774fbf750d
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-03-28 20:52:21 +0530

    Added multiplication table function

--- loop.c
+++ loop.c
@@ -5,6 +5,7 @@ char isOdd(int);
 int print_fibonacci_series(int);
 int print_oddNum_series(int);
 int print_EvenNum_series(int);
+int print_table(int, int);
 
 char isOdd(int num) {
   return num % 2;
@@ -55,18 +56,34 @@ int print_EvenNum_series(int num) {
   return 0;
 }
 
+int print_table(int multiplicand,int num) {
+  int multiplier = 1;
+  while( multiplier <= num) {
+    printf("%d * %d = %d\n", multiplicand, multiplier, multiplicand * multiplier );
+    multiplier += 1;
+  }
+  return 0;
+}
+
 int main(void) {
   int num;
-  printf("Enter Num to find its factorial : \n");
-  scanf("%d",&num);
-  printf("The factorial of %d is %d\n", num, fact(num));
+  // printf("Enter Num to find its factorial : \n");
+  // scanf("%d",&num);
+  // printf("The factorial of %d is %d\n", num, fact(num));
 
-  printf("Enter num of terms you want to print fibonacci \n");
-  scanf("%d",&num);
-  print_fibonacci_series(num);
+  // printf("Enter num of terms you want to print fibonacci \n");
+  // scanf("%d",&num);
+  // print_fibonacci_series(num);
 
-  printf("Enter num till you want to print \n");
-  scanf("%d",&num);
-  print_oddNum_series(num);
-  print_EvenNum_series(num);
+  // printf("Enter num till you want to print \n");
+  // scanf("%d",&num);
+  // print_oddNum_series(num);
+  // print_EvenNum_series(num);
+
+  int multiplicand;
+  printf("Enter multiplicand : \n");
+  scanf("%d",&multiplicand);
+  printf("Enter num : \n");
+  scanf("%d", &num);
+  print_table(multiplicand,num);
 }
