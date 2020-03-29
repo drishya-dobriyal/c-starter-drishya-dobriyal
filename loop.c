ea9edda199456d241110d288f3090d858f8edb75
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-03-29 10:25:33 +0530

    Added print odd numbers in reverse order for given step function

--- loop.c
+++ loop.c
@@ -3,12 +3,14 @@
 int fact(int);
 int print_fibonacci_series(int);
 int print_oddNum_series(int);
+int print_oddNum_series_reverse(int);
 int print_EvenNum_series(int);
 int print_table(int, int);
 int add_n_numbers(int);
 int product_n_numbers(int);
 int print_oddNum_range(int, int);
 int print_num_in_range_for_step(int, int,int);
+int sum_of_even_num(int, int);
 
 int fact(int num ) {
   if(num <= 1) {
@@ -31,27 +33,36 @@ int print_fibonacci_series(int num) {
   return 0;
 }
 
-int print_num_in_range_for_step(int from, int till,int step ) {
-  for( int num = from; num <= till; num += step){
+int print_num_in_range_for_step(int from, int to,int step ) {
+  for( int num = from; num <= to; num += step){
     printf(" %d \n",num);
   }
   return 0;
 }
 
-int print_oddNum_range( int from, int till ) {
-  printf("Odd numbers from %d to %d :\n", from , till);
+int print_oddNum_range( int from, int to ) {
+  printf("Odd numbers from %d to %d :\n", from , to);
   from = (from % 2 == 0) ? from + 1 : from;
-  return print_num_in_range_for_step(from , till, 2);
+  return print_num_in_range_for_step(from , to, 2);
 }
 
 int print_oddNum_series(int num) {
-  printf("Odd numbers till %d\n", num);
+  printf("Odd numbers to %d\n", num);
   return print_num_in_range_for_step( 1, num, 2);
 }
 
 int print_EvenNum_series(int num) {
-  printf("Even numbers till %d\n", num);
+  printf("Even numbers to %d\n", num);
   print_num_in_range_for_step(0,num, 2);
+  return 0;
+}
+
+int print_oddNum_series_reverse(int starting_term) {
+  printf("odd numbers in reverse : \n");
+  starting_term = ( starting_term % 2 ) ? starting_term : starting_term  - 1;
+  for ( int num = starting_term; num > 0; num -= 2 ){
+   printf("%d\n", num);
+  }
   return 0;
 }
 
@@ -86,6 +97,15 @@ int  product_n_numbers(int count) {
   return product;
 }
 
+int sum_of_even_num(int from,int to ) {
+  int sum = 0;
+  from = ( from % 2 ) ? (from + 1) : from;
+  for( int num = from; num <= to;  num += 2) {
+    sum += num;
+  }
+  return sum;
+}
+
 int main(void) {
   int num;
   printf("Enter Num to find its factorial : \n");
@@ -96,7 +116,7 @@ int main(void) {
   scanf("%d",&num);
   print_fibonacci_series(num);
 
-  printf("Enter num till you want to print \n");
+  printf("Enter num to you want to print \n");
   scanf("%d",&num);
   print_oddNum_series(num);
   print_EvenNum_series(num);
@@ -114,12 +134,21 @@ int main(void) {
   printf("The Sum is : %d\n", add_n_numbers(count));
   printf("The Product is : %d\n", product_n_numbers(count));
 
-  int from, till, step;
+  int from, to, step;
   printf("Enter two numbers for range for odd number of series :");
-  scanf("%d %d", &from, &till);
-  print_oddNum_range(from, till);
+  scanf("%d %d", &from, &to);
+  print_oddNum_range(from, to);
 
   printf("Enter two numbers for range for odd number of series and the step :");
-  scanf("%d %d %d", &from, &till, &step);
-  print_num_in_range_for_step(from, till, step);
+  scanf("%d %d %d", &from, &to, &step);
+  print_num_in_range_for_step(from, to, step);
+
+  printf("Enter two numbers for sum of even numbers :\n");
+  scanf("%d %d", &from, &to);
+  printf("Sum of Even numbers is : %d\n",sum_of_even_num(from, to));
+
+  int starting_term;
+  printf("Enter a number :\n");
+  scanf("%d", &starting_term);
+  print_oddNum_series_reverse(starting_term);
 }
