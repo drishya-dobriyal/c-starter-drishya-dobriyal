925c786095fd14718f569b097dad98b54ae7b6bd
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-03-29 11:17:19 +0530

    refactored print currency_breakdown function

--- currency_breakdown.c
+++ currency_breakdown.c
@@ -1,51 +1,24 @@
 #include <stdio.h>
 
 int currency_breakdown(int);
+int print_currency(int, int);
+
+int print_currency( int amount,int currency_weight) {
+  if( amount/currency_weight >= 1 ){
+    printf("%d * Rs %d \n", amount/currency_weight , currency_weight );
+  } 
+ return amount % currency_weight;
+}
 
 int currency_breakdown( int amount ) {
-  if(amount / 2000 >= 1 ) {
-    printf("%d * Rs %d \n",amount / 2000 , 2000 );
-  } 
-  amount = amount % 2000;
-
- if(amount / 500 >= 1 ) {
-    printf("%d * Rs %d \n",amount / 500 , 500 );
-  } 
-  amount = amount % 500;
-
-  if(amount / 200 >= 1 ) {
-    printf("%d * Rs %d \n",amount / 200 , 200 );
-  } 
-  amount = amount % 200;
-
-  if(amount / 100 >= 1 ) {
-    printf("%d * Rs %d \n",amount / 100 , 100 );
-  } 
-  amount = amount % 100;
-
-  if(amount / 50 >= 1 ) {
-    printf("%d * Rs %d \n",amount / 50 , 50 );
-  } 
-  amount = amount % 50;
-
-  if(amount / 10 >= 1 ) {
-    printf("%d * Rs %d \n",amount / 10 , 10 );
-  } 
-  amount = amount % 10;
-
-  if(amount / 5 >= 1 ) {
-    printf("%d * Rs %d \n",amount / 5 , 5 );
-  } 
-  amount = amount % 5;
-
-  if(amount / 5 >= 1 ) {
-    printf("%d * Rs %d \n",amount / 5 , 5 );
-  } 
-  amount = amount % 5;
-
-  if(amount / 1 >= 1 ) {
-    printf("%d * Rs %d \n",amount / 1 , 1 );
-  } 
+  int amount_left = print_currency(amount, 2000);
+  amount_left = print_currency(amount_left, 500);
+  amount_left = print_currency(amount_left, 200);
+  amount_left = print_currency(amount_left, 100);
+  amount_left = print_currency(amount_left, 50);
+  amount_left = print_currency(amount_left, 10);
+  amount_left = print_currency(amount_left, 5);
+  print_currency(amount_left, 1);
   return 0;
 }
 
