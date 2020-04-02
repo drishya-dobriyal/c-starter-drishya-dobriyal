2ee32d9b22c2da882b282fffac15194179424d88
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-04-02 14:13:35 +0530

    modified currency_breakdown

--- currency_breakdown.c
+++ currency_breakdown.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 int currency_breakdown(int, int, int *, int *);
+int print_currency_breakdown(int, int, int *, int *);
 
 int currency_breakdown(int amount, int currency_notes ,int *currency_breakdown_list, int *currency_weight_list ) {
   for(int i=0; i < currency_notes; i++) {
     currency_breakdown_list[i] =  amount/currency_weight_list[i];
     amount = amount % currency_weight_list[i];
+  }
+  return 0;
+}
+
+int print_currency_breakdown(int amount, int currency_notes ,int *currency_breakdown_list, int *currency_weight_list ) {
+  for(int i=0; i < currency_notes; i++) {
+    if(currency_breakdown_list[i]) {
+     printf("%d * Rs %d \n", currency_breakdown_list[i] , currency_weight_list[i] );
+   }
   }
   return 0;
 }
@@ -20,11 +30,6 @@ int main(void) {
 
   int  currency_breakdown_list[currency_notes];
   currency_breakdown( amount, currency_notes, currency_breakdown_list, currency_weight_list);
-  
-  for(int i=0; i < currency_notes; i++) {
-    if(currency_breakdown_list[i]) {
-     printf("%d * Rs %d \n", currency_breakdown_list[i] , currency_weight_list[i] );
-    }
-  }
+  print_currency_breakdown( amount, currency_notes, currency_breakdown_list, currency_weight_list);
   return 0;
 }
